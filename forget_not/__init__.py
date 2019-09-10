508573c48e4d28f656a7ec7a67a06f052ac37990
Luc Sinet <luc.sinet@etixgroup.com>
2019-09-10 11:32:19 +0200

    develop #comment Create user at startup.

--- forget_not/__init__.py
+++ forget_not/__init__.py
@@ -6,8 +6,10 @@ from .app.facade import AppFacade
 from .app.service.module import ServiceModule
 from .app.command.user_commands import AddUser
 
-from infra.repo.factory import Factory as RepoFactory
-from infra.repo.facade import Facade as RepoFacade
+from .domain.service.model_identity import ModelIdentity
+
+from .infra.repo.factory import Factory as RepoFactory
+from .infra.repo.facade import Facade as RepoFacade
 
 
 def _real_main(argv):
@@ -20,6 +22,8 @@ def _real_main(argv):
     repo_facade = RepoFacade(repo_factory, db)
     service_module.register_services(app_facade, repo_facade)
 
+    _create_user(repo_facade.user_repo(), app_facade.command_dispatcher())
+
     interface.run()
 
 
@@ -30,6 +34,15 @@ def _connect_to_db(name):
         print("Could not connect to database '{}': {}".format(name, e))
         sys.exit(1)
     return db
+
+
+def _create_user(user_repo, cmd_dispatcher):
+    if user_repo.count() is not 0:
+        return
+
+    id = ModelIdentity().identify_user()
+    cmd = AddUser(id, "anon")
+    cmd_dispatcher.dispatch(cmd)
 
 
 def main(argv=None):
